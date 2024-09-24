748a233714f5905a27b6f632fd909e9974961cc4
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2024-09-23 23:45:38 -0300

    new attempt to fix data-collectiing

--- pages/api/cron/data-collecting.ts
+++ pages/api/cron/data-collecting.ts
@@ -8,71 +8,82 @@ import dayjsCustomFormatter from 'dayjs/plugin/customParseFormat'
 dayjs.extend(dayjsCustomFormatter)
 
 export default async function getResults(req: NextApiRequest, res: NextApiResponse) {
-  const currentDateFormatted = dayjs().format('DD/MM/YYYY').replaceAll('/', '')
-  const { data: onlineAPIResponse } = await axios.post('https://onlinesoftware.com.br/planodecontas/apirestweb/vends/listvends.php', {
-    token: process.env.ONLINE_API_TOKEN,
-    rotina: 'listarVendas001',
-    dtinicio: currentDateFormatted,
-    dtfim: currentDateFormatted,
-  })
-  const onlineResults = onlineAPIResponse.resultado
+  try {
+    const currentDateFormatted = dayjs().format('DD/MM/YYYY').replaceAll('/', '')
+    const { data: onlineAPIResponse } = await axios.post('https://onlinesoftware.com.br/planodecontas/apirestweb/vends/listvends.php', {
+      token: process.env.ONLINE_API_TOKEN,
+      rotina: 'listarVendas001',
+      dtinicio: currentDateFormatted,
+      dtfim: currentDateFormatted,
+    })
+    const onlineResults = onlineAPIResponse.resultado
 
-  const db = await connectToDatabase()
-  const salesCollection = db.collection('sales')
-  const salesItemsCollection = db.collection('sales-items')
-  const clientsCollection: Collection<TClient> = db.collection('clients')
-
-  const clients = await clientsCollection.find({}, { projection: { nome: 1 } }).toArray()
-
-  var salesItems: any[] = []
-  var clientsToInsert: any[] = []
-  const allSales = onlineResults
-
-  const allSalesFormatted = allSales.map((sale: any, index: number) => {
-    const dateFormatted = dayjs(sale.data, 'DD/MM/YYYY').toISOString()
-    const salesItemsFormatted = sale.itens.map((saleItem: any) => ({
-      ...saleItem,
-      qtde: Number(saleItem.qtde),
-      valorunit: Number(saleItem.valorunit),
-      vprod: Number(saleItem.vprod),
-      vdesc: Number(saleItem.vdesc),
-      vcusto: Number(saleItem.vcusto),
-      baseicms: Number(saleItem.baseicms),
-      percent: Number(saleItem.percent),
-      icms: Number(saleItem.icms),
-      vfrete: Number(saleItem.vfrete),
-      vseg: Number(saleItem.vseg),
-      voutro: Number(saleItem.voutro),
-      vipi: Number(saleItem.vipi),
-      vicmsst: Number(saleItem.vicmsst),
-      vicms_desonera: Number(saleItem.vicms_desonera),
-    }))
-    const custoTotal = salesItemsFormatted.reduce((acc: number, current: any) => acc + current.vcusto, 0)
-    salesItems = [...salesItems, ...salesItemsFormatted]
-
-    const equivalentClient = clients.find((c) => c.nome == sale.cliente)
-    if (!!equivalentClient) {
-      return {
-        ...sale,
-        valor: Number(sale.valor),
-        dataVenda: dateFormatted,
-        idCliente: equivalentClient._id.toString(),
-        itens: salesItemsFormatted,
-        custoTotal,
-      }
-    } else {
-      const clientObjectId = new ObjectId()
-      clientsToInsert = [...clientsToInsert, { _id: clientObjectId, nome: sale.cliente }]
-      return { ...sale, valor: Number(sale.valor), dataVenda: dateFormatted, idCliente: clientObjectId.toString(), itens: salesItemsFormatted, custoTotal }
+    // Verificar se onlineResults é um array
+    if (!Array.isArray(onlineResults)) {
+      console.error('onlineResults não é um array:', onlineResults)
+      return res.status(500).json({ error: 'Formato de dados inesperado da API externa' })
     }
-  })
-  if (allSalesFormatted.length > 0) {
-    const salesInsertResponse = await salesCollection.insertMany(allSalesFormatted)
-    console.log(salesInsertResponse)
+
+    const db = await connectToDatabase()
+    const salesCollection = db.collection('sales')
+    const salesItemsCollection = db.collection('sales-items')
+    const clientsCollection: Collection<TClient> = db.collection('clients')
+
+    const clients = await clientsCollection.find({}, { projection: { nome: 1 } }).toArray()
+
+    var salesItems: any[] = []
+    var clientsToInsert: any[] = []
+    const allSales = onlineResults
+
+    const allSalesFormatted = allSales.map((sale: any, index: number) => {
+      const dateFormatted = dayjs(sale.data, 'DD/MM/YYYY').toISOString()
+      const salesItemsFormatted = sale.itens.map((saleItem: any) => ({
+        ...saleItem,
+        qtde: Number(saleItem.qtde),
+        valorunit: Number(saleItem.valorunit),
+        vprod: Number(saleItem.vprod),
+        vdesc: Number(saleItem.vdesc),
+        vcusto: Number(saleItem.vcusto),
+        baseicms: Number(saleItem.baseicms),
+        percent: Number(saleItem.percent),
+        icms: Number(saleItem.icms),
+        vfrete: Number(saleItem.vfrete),
+        vseg: Number(saleItem.vseg),
+        voutro: Number(saleItem.voutro),
+        vipi: Number(saleItem.vipi),
+        vicmsst: Number(saleItem.vicmsst),
+        vicms_desonera: Number(saleItem.vicms_desonera),
+      }))
+      const custoTotal = salesItemsFormatted.reduce((acc: number, current: any) => acc + current.vcusto, 0)
+      salesItems = [...salesItems, ...salesItemsFormatted]
+
+      const equivalentClient = clients.find((c) => c.nome == sale.cliente)
+      if (!!equivalentClient) {
+        return {
+          ...sale,
+          valor: Number(sale.valor),
+          dataVenda: dateFormatted,
+          idCliente: equivalentClient._id.toString(),
+          itens: salesItemsFormatted,
+          custoTotal,
+        }
+      } else {
+        const clientObjectId = new ObjectId()
+        clientsToInsert = [...clientsToInsert, { _id: clientObjectId, nome: sale.cliente }]
+        return { ...sale, valor: Number(sale.valor), dataVenda: dateFormatted, idCliente: clientObjectId.toString(), itens: salesItemsFormatted, custoTotal }
+      }
+    })
+    if (allSalesFormatted.length > 0) {
+      const salesInsertResponse = await salesCollection.insertMany(allSalesFormatted)
+      console.log(salesInsertResponse)
+    }
+    if (clientsToInsert.length > 0) {
+      const clientsInsertResponse = await clientsCollection.insertMany(clientsToInsert)
+      console.log(clientsInsertResponse)
+    }
+    return res.status(201).json('EXECUTADO COM SUCESSO')
+  } catch (error) {
+    console.error('Erro ao processar dados:', error)
+    return res.status(500).json({ error: 'Erro interno do servidor' })
   }
-  if (clientsToInsert.length > 0) {
-    const clientsInsertResponse = await clientsCollection.insertMany(clientsToInsert)
-    console.log(clientsInsertResponse)
-  }
-  return res.status(201).json('EXECUTADO COM SUCESSO')
 }
