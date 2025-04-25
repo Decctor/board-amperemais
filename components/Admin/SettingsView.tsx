c8bf0235e362384b24c61c83160c7a46ded91ca3
AmpereMaisDesenvolvimento <amperemaisdesenvolvimento@gmail.com>
2025-04-24 23:18:41 -0300

    add monetario rfm config

--- components/Admin/SettingsView.tsx
+++ components/Admin/SettingsView.tsx
@@ -16,9 +16,7 @@ function SettingsView() {
 			<div className="flex w-full flex-col items-center justify-between border-b border-gray-200 pb-2 lg:flex-row">
 				<div className="flex flex-col">
 					<h1 className="text-lg font-bold">Controle de Configurações</h1>
-					<p className="text-sm text-[#71717A]">
-						Gerencie as configurações da plataforma.
-					</p>
+					<p className="text-sm text-[#71717A]">Gerencie as configurações da plataforma.</p>
 				</div>
 			</div>
 			<RFMConfigBlock />
@@ -46,6 +44,13 @@ function RFMConfigBlock() {
 			"4": { min: 0, max: 0 },
 			"5": { min: 0, max: 0 },
 		},
+		monetario: {
+			"1": { min: 0, max: 0 },
+			"2": { min: 0, max: 0 },
+			"3": { min: 0, max: 0 },
+			"4": { min: 0, max: 0 },
+			"5": { min: 0, max: 0 },
+		},
 	});
 	useEffect(() => {
 		if (data) setInfoHolder(data);
@@ -61,23 +66,22 @@ function RFMConfigBlock() {
 	return (
 		<div className="flex w-full flex-col gap-3 rounded border border-primary bg-[#fff] p-2 shadow-sm dark:bg-[#121212]">
 			<div className="flex items-center gap-2">
-				<h1 className="text-sm font-bold leading-none tracking-tight">
-					CONFIGURAÇÃO RFM
-				</h1>
+				<h1 className="text-sm font-bold leading-none tracking-tight">CONFIGURAÇÃO RFM</h1>
 			</div>
 			<div className="w-full flex flex-col gap-2">
 				<div className="w-full flex items-center gap-2 bg-primary text-primary-foreground rounded">
-					<h1 className="w-1/3 text-center">NOTA</h1>
-					<h1 className="w-1/3 text-center">FREQUÊNCIA</h1>
-					<h1 className="w-1/3 text-center">RECÊNCIA</h1>
+					<h1 className="w-1/4 text-center">NOTA</h1>
+					<h1 className="w-1/4 text-center">FREQUÊNCIA</h1>
+					<h1 className="w-1/4 text-center">RECÊNCIA</h1>
+					<h1 className="w-1/4 text-center">MONETÁRIO</h1>
 				</div>
 				{isLoading ? <LoadingComponent /> : null}
 				{isError ? <ErrorComponent /> : null}
 				{isSuccess ? (
 					<div className="w-full flex flex-col gap-2">
 						<div className="w-full flex items-center gap-2">
-							<h1 className="w-1/3 text-center">1</h1>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<h1 className="w-1/4 text-center">1</h1>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -113,7 +117,7 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -149,10 +153,46 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÍNIMO"
+										placeholder="Preencha aqui o valor mínimo para a nota"
+										value={infoHolder.monetario[1].min}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"1": { ...prev.monetario["1"], min: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÁXIMO"
+										placeholder="Preencha aqui o valor máximo para a nota"
+										value={infoHolder.monetario[1].max}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"1": { ...prev.monetario["1"], max: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+							</div>
 						</div>
 						<div className="w-full flex items-center gap-2">
-							<h1 className="w-1/3 text-center">2</h1>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<h1 className="w-1/4 text-center">2</h1>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -188,7 +228,7 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -224,10 +264,46 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÍNIMO"
+										placeholder="Preencha aqui o valor mínimo para a nota"
+										value={infoHolder.monetario[2].min}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"2": { ...prev.monetario["2"], min: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÁXIMO"
+										placeholder="Preencha aqui o valor máximo para a nota"
+										value={infoHolder.monetario[2].max}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"2": { ...prev.monetario["2"], max: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+							</div>
 						</div>
 						<div className="w-full flex items-center gap-2">
-							<h1 className="w-1/3 text-center">3</h1>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<h1 className="w-1/4 text-center">3</h1>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -263,7 +339,7 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<NumberInput
 									label="MÍNIMO"
 									placeholder="Preencha aqui o valor mínimo para a nota"
@@ -295,10 +371,42 @@ function RFMConfigBlock() {
 									width="100%"
 								/>
 							</div>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
+								<NumberInput
+									label="MÍNIMO"
+									placeholder="Preencha aqui o valor mínimo para a nota"
+									value={infoHolder.monetario[3].min}
+									handleChange={(value) =>
+										setInfoHolder((prev) => ({
+											...prev,
+											monetario: {
+												...prev.monetario,
+												"3": { ...prev.monetario["3"], min: value },
+											},
+										}))
+									}
+									width="100%"
+								/>
+								<NumberInput
+									label="MÁXIMO"
+									placeholder="Preencha aqui o valor máximo para a nota"
+									value={infoHolder.monetario[3].max}
+									handleChange={(value) =>
+										setInfoHolder((prev) => ({
+											...prev,
+											monetario: {
+												...prev.monetario,
+												"3": { ...prev.monetario["3"], max: value },
+											},
+										}))
+									}
+									width="100%"
+								/>
+							</div>
 						</div>
 						<div className="w-full flex items-center gap-2">
-							<h1 className="w-1/3 text-center">4</h1>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<h1 className="w-1/4 text-center">4</h1>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -334,7 +442,7 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -370,10 +478,46 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÍNIMO"
+										placeholder="Preencha aqui o valor mínimo para a nota"
+										value={infoHolder.monetario[4].min}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"4": { ...prev.monetario["4"], min: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÁXIMO"
+										placeholder="Preencha aqui o valor máximo para a nota"
+										value={infoHolder.monetario[4].max}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"4": { ...prev.monetario["4"], max: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+							</div>
 						</div>
 						<div className="w-full flex items-center gap-2">
-							<h1 className="w-1/3 text-center">5</h1>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<h1 className="w-1/4 text-center">5</h1>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -409,7 +553,7 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
-							<div className="w-1/3 flex items-center gap-1 flex-col lg:flex-row">
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
 								<div className="w-full lg:w-1/2">
 									<NumberInput
 										label="MÍNIMO"
@@ -445,15 +589,48 @@ function RFMConfigBlock() {
 									/>
 								</div>
 							</div>
+							<div className="w-1/4 flex items-center gap-1 flex-col lg:flex-row">
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÍNIMO"
+										placeholder="Preencha aqui o valor mínimo para a nota"
+										value={infoHolder.monetario[5].min}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"5": { ...prev.monetario["5"], min: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+								<div className="w-full lg:w-1/2">
+									<NumberInput
+										label="MÁXIMO"
+										placeholder="Preencha aqui o valor máximo para a nota"
+										value={infoHolder.monetario[5].max}
+										handleChange={(value) =>
+											setInfoHolder((prev) => ({
+												...prev,
+												monetario: {
+													...prev.monetario,
+													"5": { ...prev.monetario["5"], max: value },
+												},
+											}))
+										}
+										width="100%"
+									/>
+								</div>
+							</div>
 						</div>
 					</div>
 				) : null}
 			</div>
 			<div className="w-full flex items-center justify-end">
-				<LoadingButton
-					loading={isPending || !isSuccess}
-					onClick={() => updateRFMConfigMutation(infoHolder)}
-				>
+				<LoadingButton loading={isPending || !isSuccess} onClick={() => updateRFMConfigMutation(infoHolder)}>
 					SALVAR
 				</LoadingButton>
 			</div>
