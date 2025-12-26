4be088f22bf230ba15660e738dcc346fcd7ffec3
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2025-12-26 10:24:05 -0300

    feat: using meta embedded signups config

--- pages/api/integrations/whatsapp/auth/index.ts
+++ pages/api/integrations/whatsapp/auth/index.ts
@@ -7,19 +7,20 @@ const handleArcticWhatsapp: NextApiHandler<any> = async (req, res) => {
 	const state = arctic.generateState();
 	const scopes = ["email", "public_profile", "whatsapp_business_management", "whatsapp_business_messaging"];
 	const url = FacebookOAuth.createAuthorizationURL(state, scopes);
-	// url.searchParams.set("config_id", process.env.NEXT_PUBLIC_META_EMBEDDED_SIGNUP_CONFIG_ID as string);
-	// url.searchParams.set("response_type", "code");
-	// url.searchParams.set("override_default_response_type", "true");
-	// url.searchParams.set(
-	// 	"extras",
-	// 	JSON.stringify({
-	// 		setup: {},
-	// 		featureType: "whatsapp_business_app_onboarding",
-	// 		sessionInfoVersion: "3",
-	// 	}),
-	// );
-	console.log("[INFO] [ARCTIC_WHATSAPP] Redirecting to:", url.toString());
-	res.redirect(url.toString().replace("v16.0", "v21.0"));
+	url.searchParams.set("config_id", process.env.NEXT_PUBLIC_META_EMBEDDED_SIGNUP_CONFIG_ID as string);
+	url.searchParams.set("response_type", "code");
+	url.searchParams.set("override_default_response_type", "true");
+	url.searchParams.set(
+		"extras",
+		JSON.stringify({
+			setup: {},
+			featureType: "whatsapp_business_app_onboarding",
+			sessionInfoVersion: "3",
+		}),
+	);
+	const finalUrl = url.toString().replace("v16.0", "v21.0");
+	console.log("[INFO] [ARCTIC_WHATSAPP] Redirecting to:", finalUrl);
+	res.redirect(finalUrl);
 };
 
 export default apiHandler({
