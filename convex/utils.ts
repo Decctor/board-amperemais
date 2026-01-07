209ec9415b5da08a55524d5abd7f705692baeab4
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2026-01-07 17:20:58 -0300

    chore: remove convex

--- convex/utils.ts
+++
@@ -1,4 +0,0 @@
-import { makeUseQueryWithStatus } from "convex-helpers/react";
-import { useQueries } from "convex/react";
-// Do this once somewhere, name it whatever you want.
-export const useConvexQuery = makeUseQueryWithStatus(useQueries);
