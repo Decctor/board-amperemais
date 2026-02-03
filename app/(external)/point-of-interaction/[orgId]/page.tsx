0b1932d6ad57bccfe02bbc09552f19e1f8f8db63
Lucas Fernandes <ampereprocessos@gmail.com>
2026-02-03 06:55:58 -0300

    feat: when user is member of the org in poinit of interaction and there is no cashback program configured, allowing him to be redirected to the config page

--- app/(external)/point-of-interaction/[orgId]/page.tsx
+++ app/(external)/point-of-interaction/[orgId]/page.tsx
@@ -1,6 +1,11 @@
 import ErrorComponent from "@/components/Layouts/ErrorComponent";
 import { OrgColorsProvider } from "@/components/Providers/OrgColorsProvider";
+import { Button } from "@/components/ui/button";
+import { getCurrentSession } from "@/lib/authentication/session";
 import { db } from "@/services/drizzle";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { MdOutlineError } from "react-icons/md";
 import PointOfInteractionContent from "./point-of-interaction-page";
 
 export default async function PointOfInteraction({ params }: { params: Promise<{ orgId: string }> }) {
@@ -9,6 +14,7 @@ export default async function PointOfInteraction({ params }: { params: Promise<{
 	if (!orgId) {
 		return <ErrorComponent msg="Oops, parâmetro inválido." />;
 	}
+	const session = await getCurrentSession();
 
 	const org = await db.query.organizations.findFirst({
 		where: (fields, { eq }) => eq(fields.id, orgId),
@@ -32,12 +38,29 @@ export default async function PointOfInteraction({ params }: { params: Promise<{
 		where: (fields, { eq }) => eq(fields.organizacaoId, orgId),
 	});
 	if (!cashbackProgram) {
-		return <ErrorComponent msg="Programa de cashback não encontrado" />;
+		return (
+			<div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+				<div className="flex flex-col items-center justify-center">
+					<MdOutlineError className="w-12 h-12 text-destructive" />
+					<p className="text-sm font-medium italic text-muted-foreground">Programa de cashback não encontrado</p>
+				</div>
+				{session?.membership?.organizacao.id === orgId ? (
+					<div className="w-full flex items-center justify-center">
+						<Button size="sm" asChild>
+							<Link href={"/dashboard/commercial/cashback-programs"}>
+								<Plus className="w-4 h-4" />
+								CONFIGURAR PROGRAMA DE CASHBACK
+							</Link>
+						</Button>
+					</div>
+				) : null}
+			</div>
+		);
 	}
 
 	return (
-		<OrgColorsProvider 
-			corPrimaria={org.corPrimaria} 
+		<OrgColorsProvider
+			corPrimaria={org.corPrimaria}
 			corPrimariaForeground={org.corPrimariaForeground}
 			corSecundaria={org.corSecundaria}
 			corSecundariaForeground={org.corSecundariaForeground}
