317589a76577cd9d63eefa9aaf0396bac1525ccd
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2026-05-15 14:58:37 -0300

    feat: update WhatsApp authorization URL parameters for improved integration
    - Added necessary search parameters to the WhatsApp authorization URL, including config_id, response_type, and extras for onboarding.
    - Updated the version in the final URL to ensure compatibility with the latest API changes.

--- app/api/integrations/whatsapp/auth/route.ts
+++ app/api/integrations/whatsapp/auth/route.ts
@@ -7,17 +7,17 @@ async function getArcticWhatsappRoute(_req: NextRequest) {
 	const state = arctic.generateState();
 	const scopes = ["whatsapp_business_management", "whatsapp_business_messaging"];
 	const url = FacebookOAuth.createAuthorizationURL(state, scopes);
-	// url.searchParams.set("config_id", process.env.NEXT_PUBLIC_META_EMBEDDED_SIGNUP_CONFIG_ID as string);
-	// url.searchParams.set("response_type", "code");
-	// url.searchParams.set("override_default_response_type", "true");
-	// url.searchParams.set(
-	// 	"extras",
-	// 	JSON.stringify({
-	// 		setup: {},
-	// 		featureType: "whatsapp_business_app_onboarding",
-	// 		sessionInfoVersion: "3",
-	// 	}),
-	// );
+	url.searchParams.set("config_id", process.env.NEXT_PUBLIC_META_EMBEDDED_SIGNUP_CONFIG_ID as string);
+	url.searchParams.set("response_type", "code");
+	url.searchParams.set("override_default_response_type", "true");
+	url.searchParams.set(
+		"extras",
+		JSON.stringify({
+			setup: {},
+			featureType: "whatsapp_business_app_onboarding",
+			sessionInfoVersion: "3",
+		}),
+	);
 	const finalUrl = url.toString().replace("v16.0", "v25.0");
 	console.log("[INFO] [ARCTIC_WHATSAPP] Redirecting to:", finalUrl);
 	return NextResponse.redirect(finalUrl);
